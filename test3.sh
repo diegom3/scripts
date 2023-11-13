1de626c260fa8dd08e4f4dd0dfa17ffb2fe5f51d
diegom3 <67805146+diegom3@users.noreply.github.com>
2023-11-13 10:48:34 -0500

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -5,20 +5,24 @@ file="example_file.txt"
 
 # Read the file line by line
 while IFS= read -r line; do
-  # Check if the line contains the array declaration for "provider"
-  if [[ $line == *"provider"*"("*")" ]]; then
-    # Extract the values inside the array
-    provider_values=($(echo "$line" | sed -n 's/.*provider=\(.*\))/\1/p' | tr -d '()'))
+  # Check if the line contains the array named "provider"
+  if [[ $line == *provider* ]]; then
+    # Extract the values from the array
+    provider_values=$(echo "$line" | grep -oP '\(.*\)' | tr -d '()' | tr -d '"' | tr -s ' ' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+    
+    # Convert the space-separated values to an array
+    IFS=" " read -r -a provider_array <<< "$provider_values"
 
-    # Define a key-value map for providers
-    provider_map=("value1" "value2" "value3")
+    # Define a key-value map
+    declare -A provider_map
+    provider_map=([value1]="mapped_value1" [value2]="mapped_value2" [value3]="mapped_value3")
 
-    # Iterate through the values in the "provider" array
-    for value in "${provider_values[@]}"; do
-      # Check if the value is in the provider_map
-      if [[ " ${provider_map[@]} " =~ " $value " ]]; then
+    # Check if the values in the provider array match the key-value map
+    for value in "${provider_array[@]}"; do
+      mapped_value="${provider_map[$value]}"
+      if [ -n "$mapped_value" ]; then
         # If a match is found, add it to another variable
-        matched_provider="$value"
+        matched_provider="$mapped_value"
         break
       fi
     done
