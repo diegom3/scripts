4c82d07292476defbfaf3c220ca3fec6c1c875bd
diegom3 <67805146+diegom3@users.noreply.github.com>
2023-11-13 09:50:16 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
 # Set the filename
-file="your_file.sh"  # Replace with your actual filename
+file="example_file.txt"
 
 # Read the file line by line
 while IFS= read -r line; do
-  # Check if the line contains the array definition for "provider"
-  if [[ $line == *"provider=("* ]]; then
-    # Extract the values of the provider array
-    provider_values=$(echo "$line" | sed -n 's/.*provider=//p' | tr -d '() ')
+  # Check if the line contains the array declaration for "provider"
+  if [[ $line == *provider*=* ]]; then
+    # Extract the values of the "provider" array
+    provider_values=$(echo "$line" | awk -F'=' '{print $2}' | tr -d '[:space:]' | sed 's/\[\(.*\)\]/\1/' | tr ',' '\n')
     
-    # Convert the space-separated values into an array
-    IFS=' ' read -r -a provider_array <<< "$provider_values"
+    # Define a key-value map
+    declare -A provider_map
+    provider_map=( ["value1"]="result1" ["value2"]="result2" ["value3"]="result3" )
 
-    # Define a map of providers and their corresponding values
-    provider_map=("value1" "value2" "value3")
-
-    # Loop through the values in the provider array
-    for value in "${provider_array[@]}"; do
+    # Loop through the provider values
+    for value in $provider_values; do
       # Check if the value is in the provider_map
-      if [[ " ${provider_map[*]} " == *" $value "* ]]; then
+      if [ -n "${provider_map[$value]}" ]; then
         # If a match is found, add it to another variable
-        matched_provider="$value"
+        matched_result="${provider_map[$value]}"
         break
       fi
     done
   fi
 done < "$file"
 
-# Display the matched provider value
-echo "Matched provider value: $matched_provider"
+# Display the matched result
+echo "Matched result: $matched_result"
