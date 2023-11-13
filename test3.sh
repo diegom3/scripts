07a4faeb4335b7003b6ee69bc41c71efaa83fadc
diegom3 <67805146+diegom3@users.noreply.github.com>
2023-11-13 10:55:34 -0500

    update

--- test3.sh
+++ test3.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
 # Set the filename
-file="example_file.txt"
+file="your_file.txt"
 
 # Read the file line by line
 while IFS= read -r line; do
-  # Check if the line contains the array "provider"
+  # Check if the line contains the array named "provider"
   if [[ $line == *provider=* ]]; then
     # Extract the values of the provider array
-    provider_values=$(echo "$line" | cut -d'=' -f2 | tr -d '[:space:]')
-
+    provider_values=$(echo "$line" | awk -F'=' '{print $2}' | tr -d '[:space:]')
+    
     # Define a key-value map for providers
-    declare -A provider_map
-    provider_map=(
-      ["value1"]="mapped_value1"
-      ["value2"]="mapped_value2"
-      ["value3"]="mapped_value3"
-      ["value4"]="mapped_value4"
-    )
+    provider_map=("value1" "mapped_value1" "value2" "mapped_value2" "value3" "mapped_value3")
 
     # Split the comma-separated values into an array
     IFS=',' read -ra values_array <<< "$provider_values"
 
-    # Iterate through the values and check against the map
+    # Iterate through the values array and append the mapped values
     for value in "${values_array[@]}"; do
-      mapped_value="${provider_map[$value]}"
-      if [ -n "$mapped_value" ]; then
-        # If a match is found, add it to another variable
-        matched_provider="$mapped_value"
-        break
-      fi
+      # Search for the value in the provider map and append the corresponding mapped value
+      for ((i = 0; i < ${#provider_map[@]}; i+=2)); do
+        if [ "$value" == "${provider_map[i]}" ]; then
+          matched_provider_values+=("${provider_map[i+1]}")
+          break
+        fi
+      done
     done
   fi
 done < "$file"
 
-# Display the matched provider value
-echo "Matched provider value: $matched_provider"
+# Display the matched provider values
+echo "Matched provider values: ${matched_provider_values[@]}"
