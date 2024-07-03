0fa9e3f659deb7ab1ebf91e15ef91807cdfe52e7
diegom3 <67805146+diegom3@users.noreply.github.com>
2024-07-03 13:51:33 -0400

    Update testfunc.py

--- testfunc.py
+++ testfunc.py
@@ -35,3 +35,29 @@ def check_required_env_vars(required_vars):
 
 check_required_env_vars(['CLOUD', 'REPO_ORGANIZATION', 'SUPPORTED_PRODUCT_WORKLOAD', 'SUPPORTED_PRODUCT_WORKLOAD_VERSION', 'TERRAFORM_DESTROY'])
 
+
+
+def tf_plan_apply(args):
+    try: 
+        if args.stage == "plan":
+            print_info("Export datadog keys from vault .......")
+            export_datadog_keys(args)
+            print_success("Proccessed datadog keys")
+            print_info("Create data dog variables for terraform ......")
+            create_datadog_variables(args)
+            print_success("created datadog variables")
+            print_info(f"moving tf var file {os.environ['TF_VARS']} to be *.auto.tfvars .....")
+            copy_auto_tfvars(args)
+            print_success("copied auto tfvars")
+            print_info(f"processing tfvars ......")
+            process_tfvars(args)
+            print_success("Finished processing tfvars")
+
+        tf_run(args)
+        print_success("Finished TF run")
+    except Exception as e:
+        print_error(f"An unexpected error occurred: {e}")
+        traceback.print_exc()  # This prints the full stack trace
+        raise  # Re-raise the exception to propagate it upwards
+
+
